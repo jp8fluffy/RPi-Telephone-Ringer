310e2141737b07790a600db8cc19355bc8ac0dcb
fangorshile <jp8shields@gmail.com>
2026-03-27 19:49:26 -0400

    Possibly working?

---
+++ src/rpitelephoneringer/Telephone.py
@@ -0,0 +1,46 @@
+import json
+import gpiozero
+from time import time
+import pyttsx3
+
+
+class Telephone:
+    def __init__(self, donation_data) -> None:
+        self.number_of_donations = donation_data[0]
+        self.donations_json = donation_data[1]
+        self.tts = pyttsx3.init()
+
+        self.relay = gpiozero.OutputDevice(pin=26)
+        self.button = gpiozero.Button(pin=2, bounce_time=1)
+
+    def ring_if_donation(self):
+
+        relay_start_time = time()
+
+        while len(self.donations_json) > 0:
+            relay_current_time = time()
+            relay_epoch = relay_current_time - relay_start_time
+
+            if relay_epoch >= 2:
+                relay_start_time = relay_current_time
+                self.relay.toggle()
+
+            if self.button.is_pressed:
+                try:
+                    donation_to_read_json = json.loads(
+                        self.return_and_remove_last_donation()
+                    )
+                    donation_name = str(donation_to_read_json["displayName"])
+                    donation_amount = str(donation_to_read_json["amount"])
+                    donation_message = str(donation_to_read_json["message"])
+                    self.tts.say(
+                        f"{donation_name} donated {donation_amount} dollars and said..... {donation_message}"
+                    )
+                except TypeError, ValueError, IndexError:
+                    print("occured when trying to read donation")
+
+    def return_and_remove_last_donation(self):
+        if len(self.donations_json) > 0:
+            last_donation = self.number_of_donations[-1]
+            self.number_of_donations = self.number_of_donations[:-1]
+            return last_donation
--- src/rpitelephoneringer/main.py
+++ src/rpitelephoneringer/main.py
@@ -1,43 +1,20 @@
-import gpiozero
-import json
 from rpitelephoneringer.DonoTracker import DonationTracker
-import rpitelephoneringer.ringer as ringer
+from rpitelephoneringer.Telephone import Telephone
 from time import time
-from platformdirs import user_cache_dir
-from gtts import gTTS
-from os import system, path
-import shutil
-
-cachedir = user_cache_dir("rpitelephoneringer", "Johnpaul Shields", ensure_exists=True)
 
 url = "https://www.extra-life.org/api/1.6/participants/minismeef/donations"
-tracker = DonationTracker(url, cachedir + "/donos.json")
+tracker = DonationTracker(url)
 start_time = time()
-
-# relay = gpiozero.OutputDevice(26)
-# read_donation_button = gpiozero.Button(2)
-
-
-def create_and_play_tts(text: str, dir, dono_number: int):
-    file_path = str(dir + "/" + str(dono_number) + ".mp3")
-    tts = gTTS(text, lang="en", slow=True)
-    tts.save(file_path)
-    system("mpg123 " + str(file_path))
 
 
 if __name__ == "__main__":
-    if path.exists(cachedir) and path.isdir(cachedir):
-        shutil.rmtree(cachedir)
-        print("deleted cache")
     while True:
         current_time = time()
-        if current_time - start_time >= 15:
-            start_time = current_time
-            new_donos = tracker.get_new_donations()
-            print(
-                f"Number of new donations {new_donos[1][0]['message']} {json.dumps(new_donos[1], indent=4)}"
-            )
-            create_and_play_tts(
-                new_donos[1][0]["message"], str(cachedir + "/"), new_donos[0]
-            )
-            print("done")
+        time_epoch = current_time - start_time
+
+        if time_epoch >= 15:
+            donation_data = tracker.get_new_donations()
+
+        if donation_data[0] > 0:
+            telephone = Telephone(donation_data)
+            telephone.ring_if_donation()
